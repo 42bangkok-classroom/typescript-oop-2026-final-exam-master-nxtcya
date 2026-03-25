04353616aecfe7d0856bfd57b2ae10ff35556b37
nxtcya <quinn3nxt@gmail.com>
2026-03-25 12:10:56 +0700

    p01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from './interfaces/response.interface';
+import { ApiExceptionFilter } from './common/api-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(){
+    const data = this.appService.getHello()
+    return (success: true, message:"Hello NestJS", data:{})
+
+   // return this.appService.getHello();
   }
 }
